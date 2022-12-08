8b6015308584b8d06d3a5020a9a622f8270f5978
ssyedhamed <connectwithsyedhamed@gmail.com>
2022-12-09 01:04:14 +0530

    project setup

---
+++ src/main/java/com/ssyedhamed/cdrive/ContactsDriveApplication.java
@@ -0,0 +1,13 @@
+package com.ssyedhamed.cdrive;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ContactsDriveApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ContactsDriveApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ssyedhamed/cdrive/ContactsDriveApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ssyedhamed.cdrive;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ContactsDriveApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
